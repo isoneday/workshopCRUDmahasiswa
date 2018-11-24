89b344399134953274bb312ce5c9eaaa77515f2b
udakita <udakita11@gmail.com>
2018-11-24 11:37:05 +0700

    Initial commit

---
+++ app/src/main/java/com/isoneday/myapplication/network/RestApi.kt
@@ -0,0 +1,24 @@
+package com.isoneday.myapplication.network
+
+interface RestApi {
+
+//    @FormUrlEncoded
+//    @POST("login.php")
+//    fun loginuser(@Field("username") username:String,@Field("password") password:String,@Field("level") level:String?): Call<ResponseLogin>
+//
+//  @FormUrlEncoded
+//    @POST("register.php")
+//    fun registeruser(@Field("username") username:String,@Field("password") password:String,@Field("level") level:String?): Call<ResponseLogin>
+//
+//    @FormUrlEncoded
+//    @POST("add_mahasiswa.php")
+//    fun insertMahasiswa(@Field("nim") nim:String,@Field("nama") nama:String,@Field("alamat") alamat:String?,@Field("jurusan") jurusan:String?): Call<ResponseLogin>
+//
+//    @GET("get_mahasiswa.php")
+//    fun getListMahasiswa(): Call<ResponseListMahasiswa>
+
+//    @FormUrlEncoded
+//   @GET("api/v1/json/1/eventsnextleague.php")
+//    fun getNextMatch(@Query("id") leagueid:Int): Call<ResponseLogin>
+
+}
